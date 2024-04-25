ef5b1d373d229e72d03eb6ac2aaff7baa43e7811
roshankc00 <120471898+roshankc00@users.noreply.github.com>
2024-04-26 00:40:21 +0545

    feat:added the user authentication related endpints

--- users/serializers.py
+++ users/serializers.py
@@ -20,8 +20,15 @@ class UserSerializers(serializers.ModelSerializer):
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
     
+    
 class UserLoginSerializer(serializers.ModelSerializer):
     email=serializers.EmailField(max_length=200)
     class Meta:
         model=User
         fields=['email','password']
+        
+class UserProfileSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = ['id', 'email', 'name','phoneNumber']
+        
--- users/views.py
+++ users/views.py
@@ -2,11 +2,13 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from .serializers import UserSerializers,UserLoginSerializer
+from .serializers import UserSerializers,UserLoginSerializer,UserProfileSerializer
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
 from .formatResponse import UserRenderer
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .models import User
 def get_tokens_for_user(user):
   refresh = RefreshToken.for_user(user)
   return {
@@ -40,6 +42,16 @@ class UserLoginView(APIView):
                 return Response({'success':False, 'message':"User not found"},status=status.HTTP_404_NOT_FOUND)     
         else:
           return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+                    
+class UserProfileView(APIView):
+  renderer_classes = [UserRenderer]
+  authentication_classes=[JWTAuthentication]
+  permission_classes = [IsAuthenticated]
+  def get(self, request, format=None):
+    print(request.user)
+    serializer = UserProfileSerializer(request.user)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
             
-            
-        
